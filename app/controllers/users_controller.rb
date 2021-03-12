eff5e2412afc0dcdada3b64db640a3e4c755e825
massive2351 <massive2351@gmail.com>
2021-03-12 01:16:18 +0000

    途中

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    
+    
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
+  devise_for :users
+  resources :users, only: [:new, :create, :index, :show, :destroy]
+  resources :books
 end
